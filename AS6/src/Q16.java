965ec9f3521630f1c8c61bcb3441682ef399e09c
Sritish <sritishkumargouda@gmail.com>
2025-01-05 22:42:23 +0530

    sem 3 csw 1

---
+++ AS6/src/Q16.java
@@ -0,0 +1,25 @@
+public class Q16 {
+    public static void main(String[] args) {
+        int start = 10; // starting range
+        int end = 50;   // ending range
+
+        System.out.println("Prime numbers between " + start + " and " + end + " are:");
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
